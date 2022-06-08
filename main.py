48b21a097359331940ec514a762f45bab1b37858
verbal-noun <kaifahsan420@gmail.com>
2022-06-09 00:03:48 +1000

    Created a function to process the scrapped data

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import random
+import string
 import requests
 from bs4 import BeautifulSoup
+# from dataclasses import dataclass
 
+# Base URL to be scrapped
 url = "https://www.imdb.com/chart/top"
+
+# Data class to store the info
+
+
+# @dataclass
+# class Movie:
+#     title: str
+#     year: int
+#     rating: float
+#     actors: list
 
 # The main function
 
@@ -17,10 +30,35 @@ def main():
     soup = BeautifulSoup(html, 'html.parser')
     # Select the table data which contains movie info
     movie_tags = soup.select('td.titleColumn')
-    movie_0 = movie_tags[0]
+    # Selecting the ratings
+    rating_tags = soup.select('td.posterColumn span[name=ir]')
 
-    movie_0_year = movie_0.find("span", class_='secondaryInfo').text
-    print(movie_0_year)
+    # Generating the data from the scrapped elements
+    movies_list = movie_list_generator(movie_tags, rating_tags)
+
+
+# A class to give out the
+
+
+def movie_list_generator(movie_tags, rating_tags):
+    movies_dict = {}
+
+    for i in range(len(movie_tags)):
+        movie = movie_tags[i]
+        # Use the find method to identify elements
+        innerTag = movie.find('a')
+        movie_name = innerTag.text
+        # Attributes within is stored as a list.
+        actor_list = innerTag['title']
+        year = movie.find("span", class_='secondaryInfo').text
+        # The values are by default stored in string
+        rating = float(rating_tags[i]['data-value'])
+        # Round to one decimal place
+        rating = round(rating, 1)
+
+        movies_dict[movie_name] = (rating, year, actor_list)
+
+    return movies_dict
 
 
 if __name__ == '__main__':
